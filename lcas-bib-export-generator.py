327c180f6ccc0eb3d1a24b6d7353219758eecff0
Marc Hanheide <marc@hanheide.net>
2024-07-19 17:58:43 +0100

    first working prototype

--- lcas-bib-export-generator.py
+++ lcas-bib-export-generator.py
@@ -1,45 +1,14 @@
 #!/usr/bin/env python
 
+from orcid_to_bibtex import get_orcid_works, parse_and_format_bib
+from asyncio import run
+
 from requests import get
 from sys import stderr
 
-# old URL using IDs that is NOT working reliably:
-# http://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lirolem_BibTeX.bib?screen=Search&dataset=archive&_action_export=1&output=BibTeX&exp=0%7C1%7C-date%2Fcreators_name%2Ftitle%7Carchive%7C-%7Ccreators_id%3Acreators_id%3AANY%3AIN%3A002146+801704+801872+801929+003092+002165+001928+802799+002604+504752+504299+002325%7Ctype%3Atype%3AANY%3AEQ%3Aarticle+review+book_section+monograph+conference_item+book+thesis+dataset%7C-%7Ceprint_status%3Aeprint_status%3AANY%3AEQ%3Aarchive%7Cmetadata_visibility%3Ametadata_visibility%3AANY%3AEQ%3Ashow&n=
-
-staff = [
-    'Hanheide, Marc',
-    'Duckett, Tom',
-    'Sklar, Elizabeth',
-    'Saaj, Mini',
-    'Elgeneidy',
-    'Esfahani',
-    'Bosilj',
-    'Calisti, Marcello',
-    'Das, Gautham',
-    'Gao, Junfeng',
-    'Guevara, Leonardo',
-    'Maleki, Sepehr',
-    'Al-Khafajiy',
-    'Polydoros', 
-    'Yue, Shigang',
-    'Bellotto, Nicola',
-    'Baxter, Paul',
-    'Cielniak, Grzegorz',
-    'Cuayahuitl, Heriberto',
-    'Fox, Charles',
-    'Parsons, Simon',
-    'Pearson, Simon',
-    'Bochtis',
-    'Polvara, Riccardo',
-    'del Duchetto, Francesco',
-    'Klimchik, Alexandr',
-    'Rai, Mini',
-    'Harman, Helen',
-    'Zied, Tayeb'
-]
 
 # recent
-years = list(range(2012,2025))
+#years = list(range(2012,2025))
 
 def quote_name(n):
     return '%%22%s%%22' % n.replace(',','%2C').replace(' ','+')
@@ -51,12 +20,12 @@ def quote_names(ns):
 #http://eprints.lincoln.ac.uk/cgi/search/archive/advanced?screen=Search&dataset=archive&_action_search=Search&documents_merge=ALL&documents=&title_merge=ALL&title=&documents.title_merge=ALL&documents.title=&creators_name_merge=ANY&creators_name=Hanheide+Duckett+Saaj+Sklar+Yue+Bellotto+Baxter&creators_id_merge=ALL&creators_id=&abstract_merge=ALL&abstract=&date_online=&date_accepted=&date=2019-&documents.description_merge=ALL&documents.description=&keywords_merge=ALL&keywords=&subjects_merge=ANY&divisions_merge=ANY&editors_name_merge=ALL&editors_name=&refereed=EITHER&publication_merge=ALL&publication=&projects_merge=ALL&projects=&satisfyall=ALL&order=-date%2Fcreators_name%2Ftitle
 #http://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lincoln_BibTeX.bib?screen=Search&dataset=archive&_action_export=1&output=BibTeX&exp=0%7C1%7C-date%2Fcreators_name%2Ftitle%7Carchive%7C-%7Ccreators_name%3Acreators_name%3AANY%3AEQ%3AHanheide+Duckett+Saaj+Sklar+Yue+Bellotto+Baxter%7Cdate%3Adate%3AALL%3AEQ%3A2019-%7C-%7Ceprint_status%3Aeprint_status%3AANY%3AEQ%3Aarchive%7Cmetadata_visibility%3Ametadata_visibility%3AANY%3AEQ%3Ashow&n=&cache=11353462
 
-url_pattern='http://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lirolem_BibTeX.bib?screen=Search&dataset=archive&_action_export=1&output=BibTeX&exp=0%%7C1%%7C-%%7Ccreators_name%%3Acreators_name%%3AANY%%3AIN%%3A%s%%7Cdate%%3Adate%%3AALL%%3AEQ%%3A%s'
+# url_pattern='http://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lirolem_BibTeX.bib?screen=Search&dataset=archive&_action_export=1&output=BibTeX&exp=0%%7C1%%7C-%%7Ccreators_name%%3Acreators_name%%3AANY%%3AIN%%3A%s%%7Cdate%%3Adate%%3AALL%%3AEQ%%3A%s'
 
 
-'https://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lincoln_AllRSS.rss?screen=Search&amp;dataset=archive&amp;_action_export=1&amp;output=AllRSS&amp;exp=0%7C1%7C-date%2Fcreators_name%2Ftitle%7Carchive%7C-%7Ccreators_name%3Acreators_name%3AALL%3AEQ%3AHanheide'
+# 'https://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lincoln_AllRSS.rss?screen=Search&amp;dataset=archive&amp;_action_export=1&amp;output=AllRSS&amp;exp=0%7C1%7C-date%2Fcreators_name%2Ftitle%7Carchive%7C-%7Ccreators_name%3Acreators_name%3AALL%3AEQ%3AHanheide'
 
-rss_url_pattern='http://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lincoln_RSS2.xml?screen=Search&dataset=archive&_action_export=1&output=RSS2&exp=0%%7C1%%7C-%%7Ccreators_name%%3Acreators_name%%3AANY%%3AIN%%3A%s'
+# rss_url_pattern='http://eprints.lincoln.ac.uk/cgi/search/archive/advanced/export_lincoln_RSS2.xml?screen=Search&dataset=archive&_action_export=1&output=RSS2&exp=0%%7C1%%7C-%%7Ccreators_name%%3Acreators_name%%3AANY%%3AIN%%3A%s'
 
 
 def highlight_names(names):
@@ -84,36 +53,64 @@ def rss_url(staff):
 def get_file(bibtex_url):
     return get(bibtex_url, verify=False, timeout=200).text
 
-years.reverse()
+#years.reverse()
+async def main():
 
-with open('wordpress.html','w') as html_file:
-    print('<p>Download the <a href="%s" target="_blank">BibTeX file of all L-CAS publications</a></p>' % (
-        pubs_year_url('', staff)
-    ), file=html_file)
+    staff_dict = {
+        'Hanheide, Marc': '0000-0001-7728-1849',
+        'Duckett, Tom': None,
+        'Sklar, Elizabeth': None,
+        'Saaj, Mini': None,
+        'Elgeneidy': None,
+        'Esfahani': None,
+        'Bosilj': None,
+        'Calisti, Marcello': None,
+        'Das, Gautham': None,
+        'Gao, Junfeng': None,
+        'Guevara, Leonardo': None,
+        'Maleki, Sepehr': None,
+        'Al-Khafajiy': None,
+        'Polydoros': None, 
+        'Yue, Shigang': None,
+        'Bellotto, Nicola': None,
+        'Baxter, Paul': None,
+        'Cielniak, Grzegorz': '0000-0002-6299-8465',
+        'Cuayahuitl, Heriberto': None,
+        'Fox, Charles': None,
+        'Parsons, Simon': None,
+        'Pearson, Simon': None,
+        'Bochtis': None,
+        'Polvara, Riccardo': None,
+        'del Duchetto, Francesco': None,
+        'Klimchik, Alexandr': None,
+        'Rai, Mini': None,
+        'Harman, Helen': None,
+        'Zied, Tayeb': None
+    }
 
-    with open('lcas.bib', 'w') as all_bib:
-        bibtex = get_file(pubs_year_url('', staff))
-        all_bib.write(bibtex)
 
-    for year in years:
-        print("<h2>%s</h2>" % str(year), file=html_file)
-        bibtex_url = pubs_year_url(year, staff)
-        print('generating for year %d using %s' % (year, bibtex_url), file=stderr)
+    with open('wordpress.html','w') as html_file:
+
+        with open('lcas.bib', 'w') as all_bib:
+            bibs = []
+            for (staff, orcid) in staff_dict.items():
+                if orcid:
+                    print('process staff %s with id %s' % (staff, orcid), file=stderr)
+                    bibs.extend(await get_orcid_works(orcid, max_dls=5))
+            bib = "".join(set(bibs))            
+            all_bib.write(
+                parse_and_format_bib(bib)
+            )
+
         print(shortcode_pattern % (
-            bibtex_url,
-            highlight_names(staff),
-            highlight_names(staff)
+            'https://raw.githubusercontent.com/LCAS/eprint_cache/main/lcas.bib',
+            highlight_names(staff_dict.keys()),
+            highlight_names(staff_dict.keys())
         ), file=html_file)
 
-        bibtex = get_file(bibtex_url)
-        with open('%d.bib' % year, 'w') as bibtex_file:
-            bibtex_file.write(bibtex)
 
 
-print('-------------------------------')
-with open('lcas.rss','w') as rss_file:
-    rssdata = get_file(rss_url(staff))
-    rss_file.write(rssdata)
-    print(rss_url(staff))
 
 
+if __name__ == "__main__":
+    run(main())
